fb52c718c5066df37e97e7bce221295612e06a6a
SMABofficial <syedmuzammilahmadbukhari6@gmail.com>
2023-05-24 11:33:18 +0500

    dimple to do list

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,24 +2,40 @@ import { useState } from "react";
 
 
 function Home(){
-    const [fruits , setFruits] = useState (["Apple" , "jusice", "water" , "peach"]) ;
+    const [fruits , setFruits] = useState ([]) ;
     const [textInput , setTextInput] = useState ("");
 
 
 
 const inputUpdate = (event) => {
-console.log(event.target.value)
+   
+    setTextInput(event.target.value);
 
 }
 const addItem = () => {
+
+    
+    
+    const finalItems = [...fruits , textInput];
+    setFruits(finalItems)
+    setTextInput("")
+
 
 }
 
 
     return(
         <div>
-           <input type="text" onChange={inputUpdate}/>
-           <button onChange={addItem}> add</button>
+           <input type="text" onChange={inputUpdate} value={textInput} autoFocus />
+           <button onClick={addItem}> add</button>
+
+          <ul>
+            {
+                fruits.map((item , index) => {
+                    return <li key={index}>{item}</li>
+                })
+            }
+          </ul>
         </div>
     )
 }
